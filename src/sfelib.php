523b8a8f20a4575d46cfd7e457fa58504dabb1e6
botnyx <chlsbkrs@gmail.com>
2019-03-14 11:43:33 +0100

    ..

--- src/sfelib.php
+++ src/sfelib.php
@@ -1,7 +1,35 @@
 <?php
 
 
-namespace Botnyx\\Sfe\\Javascript;
+namespace Botnyx\Sfe\Javascript;
 
 
+
+class sfelib {
+	
+	
+	function __construct($root,$tmp=false){
+		
+		$root;
+		if( $tmp==false){
+			$tmp = sys_get_temp_dir();
+		}
+		
+		
+		
+		$loader = new \Twig\Loader\FilesystemLoader($root."/vendor/botnyx/sfe-backend-js/src/sfe/");
+		$this->twig = new \Twig\Environment($loader, [
+			'cache' => $tmp.'/sfe-js',
+		]);
+
+		
+		
+	}
+	
+	function get ($array=array()){
+		return $this->twig->render('sfe.js', $array );
+	}
+	
+	
+}
 
